d9e24c9d9bec5f24cdf5c3fc2b70a3b6166f43c0
leoer <leoer@DESKTOP-HBVJVS0>
2020-11-03 18:24:16 -0800

    for what? starring badger badger, my ages, and bananas!

---
+++ src/_05_for_loops/_2_badgers/Badger.java
@@ -0,0 +1,36 @@
+package _05_for_loops._2_badgers;
+
+public class Badger {
+public static void main(String[] args) {
+	for(int i= 1; i<13; i++) {
+		System.out.print("Badger"+", ");
+	}
+	System.out.println();
+		for(int r= 1; r<3; r++) {
+			System.out.print("Mushroom"+", ");
+	}
+		System.out.println();
+		System.out.println();
+		for(int i= 1; i<13; i++) {
+			System.out.print("Badger"+", ");
+		}
+		System.out.println();
+			for(int r= 1; r<3; r++) {
+				System.out.print("Mushroom"+", ");
+		}
+			System.out.println();
+			System.out.println();
+			for(int a= 1; a<2; a++) {
+				System.out.print(" A Snake!!!");
+		}
+}
+}
+
+
+//Badger, Badger, Badger, Badger, Badger, Badger, Badger,Badger, Badger,Badger, Badger, Badger
+//Mushroom, Mushroom
+
+//Badger, Badger, Badger,Badger, Badger, Badger, Badger,Badger, Badger, Badger, Badger, Badger
+//Mushroom, Mushroom
+
+//A Snake!!! 
---
+++ src/_05_for_loops/_3_bananas_4eva/Bannana.java
@@ -0,0 +1,12 @@
+package _05_for_loops._3_bananas_4eva;
+
+public class Bannana {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i=1; 1<1001; i++) {
+	System.out.println("bannana");
+}
+	}
+
+}
